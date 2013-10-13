120d78cadbbef12f22e5ccb0d3fbe575e07f75b1
jverein <jverein>
2013-10-13 10:45:11 +0000

    Bugfix Encoding blz.txt

--- src/de/jost_net/OBanToo/SEPA/BankenDaten/BLZDatei.java
+++ src/de/jost_net/OBanToo/SEPA/BankenDaten/BLZDatei.java
@@ -13,6 +13,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 public class BLZDatei
 {
@@ -20,7 +21,8 @@ public class BLZDatei
 
   public BLZDatei(InputStream is)
   {
-    br = new BufferedReader(new InputStreamReader(is));
+    br = new BufferedReader(new InputStreamReader(is,
+        Charset.forName("ISO-8859-1")));
   }
 
   public BLZSatz getNext() throws IOException
